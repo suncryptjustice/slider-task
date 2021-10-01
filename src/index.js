c8033fe49fedab26e68ef459b3d4af52bc9bc8b8
unknown <suncrypt@gmail.com>
2021-10-01 06:32:46 +0300

    add scroll polyfill for safari

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import smoothscroll from "smoothscroll-polyfill";
+smoothscroll.polyfill();
 
 ReactDOM.render(
   <React.StrictMode>
