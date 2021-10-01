122f491cfaf51f573b3a1d0d2048811b7705d751
unknown <suncrypt@gmail.com>
2021-10-01 06:15:08 +0300

    image fixes 2.0

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import MainSlider from "./components/MainSlider";
 import Page from "./components/Page";
 import Layout from "./components/Layout/Layout";
+import img1 from "./assets/1.svg";
+import img2 from "./assets/2.svg";
+import img3 from "./assets/3.svg";
+import img4 from "./assets/4.svg";
 
 function App() {
   const [activePage, setActivePage] = React.useState(0);
@@ -20,28 +24,28 @@ function App() {
         <Page style={{ backgroundColor: "red" }}>
           <img
             style={{ height: "100%", width: "100%", objectFit: "scale-down" }}
-            src="/undraw_happy_music_g6wc.svg"
+            src={img1}
             alt="bird listen music"
           />
         </Page>
         <Page style={{ backgroundColor: "teal" }}>
           <img
             style={{ height: "100%", width: "100%", objectFit: "scale-down" }}
-            src="/undraw_instruction_manual_cyae.svg"
+            src={img2}
             alt="interior"
           />
         </Page>
         <Page style={{ backgroundColor: "orange" }}>
           <img
             style={{ height: "100%", width: "100%", objectFit: "scale-down" }}
-            src="/undraw_refreshing_beverage_td3r.svg"
+            src={img3}
             alt="juice"
           />
         </Page>
         <Page style={{ backgroundColor: "purple" }}>
           <img
             style={{ height: "100%", width: "100%", objectFit: "scale-down" }}
-            src="/undraw_xmas_surprise_57p1.svg"
+            src={img4}
             alt="gift"
           />
         </Page>
--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
+import pirate from "../../assets/pirate.gif";
 
 export const Footer = React.memo(() => {
   return (
     <div className="Footer">
       <p className="truncate">{` © ${new Date().getFullYear()} Suncrypt Workflow `}</p>
-      <img className="FooterImg" src="/pirateparrot.gif" alt="pirate" />
+      <img className="FooterImg" src={pirate} alt="pirate" />
     </div>
   );
 });
